30d4dffbab59ef1ae51948a667eba903cf194527
dongkyuk0419 <dongkyuk0419@gmail.com>
2018-05-03 21:51:16 -0400

    nothing

--- Dig/PS3/PS3.m
+++ Dig/PS3/PS3.m
@@ -3,3 +3,12 @@
 % ECE 418 Digital Video
 % Professor Fontaine
 clc; clear all; close all;
+%% 0. Prelim
+video_truth = VideoReader('traffic.mj2');
+% implay('traffic.mj2')
+frames = zeros(video_truth.Height, video_truth.Width, video_truth.FrameRate*video_truth.Duration);
+% 
+while
+for i = 1:size(frames,3)
+    frames(:,:,i) = uint8(rgb2gray(readFrame(video_truth,i)));
+end
